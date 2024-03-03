b1529e784900aa47a3029df7667165569993bd37
aditya-sarawat <adityasarawat@gmail.com>
2024-03-03 12:19:18 +0530

    Code restructure and formatting

--- motivational_content/video_processor.py
+++ motivational_content/video_processor.py
@@ -13,30 +13,21 @@ from moviepy.video.fx.all import mirror_x
 BASE_PATH = "./motivational_content/__temp__/"
 TEMP_VIDEO_PATH = "./motivational_content/__temp__/temp_video.mp4"
 
-def reverse_and_append(video_path, output_path):
-    with VideoFileClip(video_path) as video_clip:
-        video_clip = video_clip.resize(height=480)
-        reversed_clip = video_clip.fx(mirror_x)
-        final_clip = concatenate_videoclips([video_clip, reversed_clip])
-        final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac", bitrate="1000k")
-
-def adjust_video_length(input_path, output_path, target_duration=15):
-    video_clip = VideoFileClip(input_path)
-
-    trimmed_clip = video_clip.subclip(0, target_duration)
-    trimmed_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
-    
-    video_clip.close()
-    trimmed_clip.close()
 
 def download_video(video_url):
     try:
         print("Downloading video...")
         response = requests.get(video_url, stream=True)
         response.raise_for_status()
-        total_size = int(response.headers.get('content-length', 0))
+        total_size = int(response.headers.get("content-length", 0))
         block_size = 2048
-        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc='Video Download', position=0)
+        progress_bar = tqdm(
+            total=total_size,
+            unit="B",
+            unit_scale=True,
+            desc="Video Download",
+            position=0,
+        )
 
         with open(TEMP_VIDEO_PATH, "wb") as video_file:
             for data in response.iter_content(block_size):
@@ -50,7 +41,18 @@ def download_video(video_url):
     except requests.exceptions.RequestException as e:
         print(f"Error during video download: {e}")
         return False
-    
+
+
+def trim_and_rename_video(video_path, target_duration):
+    temp_path = BASE_PATH + "trimmer.mp4"
+    video_clip = VideoFileClip(video_path)
+    trimmed_clip = video_clip.subclip(0, target_duration)
+    trimmed_clip.write_videofile(temp_path, codec="libx264", audio_codec="aac")
+    video_clip.close()
+    trimmed_clip.close()
+    os.rename(temp_path, video_path)
+
+
 def check_and_process_video_for_quote(video_path, quote):
     try:
         video_duration = VideoFileClip(video_path).duration
@@ -61,21 +63,16 @@ def check_and_process_video_for_quote(video_path, quote):
         print(f"Video length adjusted to {required_duration} sec")
 
         if video_duration < required_duration:
-            # Extend the video length
-            trimmed_video_path = BASE_PATH + "trimmer.mp4"
-            adjust_video_length(video_path, trimmed_video_path, target_duration=required_duration)
-            os.rename(trimmed_video_path, video_path)
+            trim_and_rename_video(video_path, target_duration=required_duration)
         else:
-            # Trim the video length
-            trimmed_video_path = BASE_PATH + "trimmer.mp4"
-            adjust_video_length(video_path, trimmed_video_path, target_duration=required_duration)
-            os.rename(trimmed_video_path, video_path)
-        
+            trim_and_rename_video(video_path, target_duration=required_duration)
+
         return required_duration
     except Exception as e:
         print(f"Error during video processing: {e}")
 
-def crop_video(video_path, output_path, target_width, target_height):
+
+def crop_and_resize_video(video_path, output_path, target_width, target_height):
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -87,22 +84,25 @@ def crop_video(video_path, output_path, target_width, target_height):
     target_aspect_ratio = target_width / target_height
     original_aspect_ratio = original_width / original_height
 
-    if target_aspect_ratio < original_aspect_ratio:
-        crop_width = int(original_height * target_aspect_ratio)
-        crop_start = (original_width - crop_width) // 2
-        crop_end = crop_start + crop_width
-    else:
-        crop_width = original_width
-        crop_start = 0
-        crop_end = original_width
+    crop_width = min(original_width, int(original_height * target_aspect_ratio))
+    crop_start = (original_width - crop_width) // 2
+    crop_end = crop_start + crop_width
 
-    with tqdm(total=frame_count, unit='frames', unit_scale=True, desc='Cropping Video', position=0) as pbar:
+    with tqdm(
+        total=frame_count,
+        unit="frames",
+        unit_scale=True,
+        desc="Cropping Video",
+        position=0,
+    ) as pbar:
         while True:
             ret, frame = cap.read()
             if not ret:
                 break
 
-            frame_cropped = cv2.resize(frame[:, crop_start:crop_end], (target_width, target_height))
+            frame_cropped = cv2.resize(
+                frame[:, crop_start:crop_end], (target_width, target_height)
+            )
             frame_resized = cv2.cvtColor(frame_cropped, cv2.COLOR_BGR2BGRA)
 
             out.append_data(frame_resized)
@@ -111,10 +111,12 @@ def crop_video(video_path, output_path, target_width, target_height):
         out.close()
         cap.release()
 
-    print("Video cropped successfully.")
+    print("Video cropped and resized successfully.")
+
 
 def apply_blur(frame, blur_strength=2.0):
     return cv2.GaussianBlur(frame, (0, 0), blur_strength)
+
 
 def apply_blur_to_video(video_path, output_path, blur_strength):
     cap = cv2.VideoCapture(video_path)
@@ -123,7 +125,13 @@ def apply_blur_to_video(video_path, output_path, blur_strength):
 
     out = imageio.get_writer(output_path, fps=fps, macro_block_size=None)
 
-    with tqdm(total=frame_count, unit='frames', unit_scale=True, desc='Applying Blur', position=2) as pbar:
+    with tqdm(
+        total=frame_count,
+        unit="frames",
+        unit_scale=True,
+        desc="Applying Blur",
+        position=2,
+    ) as pbar:
         while True:
             ret, frame = cap.read()
             if not ret:
@@ -137,17 +145,20 @@ def apply_blur_to_video(video_path, output_path, blur_strength):
     out.close()
     cap.release()
 
+
 def add_overlay(frame_pil):
     overlay = Image.new("RGBA", frame_pil.size, (0, 0, 0, int(255 * 0.4)))
     frame_pil = Image.alpha_composite(frame_pil.convert("RGBA"), overlay)
 
     return frame_pil.convert("RGB")
 
+
 def textsize(text, font):
     im = Image.new(mode="P", size=(0, 0))
     draw = ImageDraw.Draw(im)
     _, _, width, height = draw.textbbox((0, 0), text=text, font=font)
     return width, height
+
 
 def add_text_to_frame(frame, text, font, video_dimensions, duration, video_length):
     draw = ImageDraw.Draw(frame)
@@ -181,7 +192,9 @@ def add_text_to_frame(frame, text, font, video_dimensions, duration, video_lengt
         font_resized = ImageFont.truetype(font.path, new_font_size)
         text_width_resized, text_height_resized = textsize(line, font_resized)
         x_position = (video_dimensions[0] - text_width_resized) // 2
-        draw.text((x_position, y_position), line, font=font_resized, fill=(255, 255, 255, 255))
+        draw.text(
+            (x_position, y_position), line, font=font_resized, fill=(255, 255, 255, 255)
+        )
         y_position += text_height_resized
 
         total_duration += line_duration
@@ -191,11 +204,16 @@ def add_text_to_frame(frame, text, font, video_dimensions, duration, video_lengt
 
     return np.array(frame)
 
+
 def process_cropped_video(video_path, quote, selected_font, video_length):
     try:
         cap = cv2.VideoCapture(video_path)
         fps = cap.get(cv2.CAP_PROP_FPS)
-        out = imageio.get_writer(os.path.join("./reels", generate_unique_filename("mp4")), fps=fps, macro_block_size=None)
+        out = imageio.get_writer(
+            os.path.join("./reels", generate_unique_filename("mp4")),
+            fps=fps,
+            macro_block_size=None,
+        )
 
         print("Processing video...")
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -207,7 +225,13 @@ def process_cropped_video(video_path, quote, selected_font, video_length):
 
         duration = 0.0
 
-        with tqdm(total=frame_count, unit='frames', unit_scale=True, desc='Video Processing', position=1) as pbar:
+        with tqdm(
+            total=frame_count,
+            unit="frames",
+            unit_scale=True,
+            desc="Video Processing",
+            position=1,
+        ) as pbar:
             while True:
                 ret, frame = cap.read()
                 if not ret:
@@ -215,7 +239,14 @@ def process_cropped_video(video_path, quote, selected_font, video_length):
 
                 frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
                 frame_pil = add_overlay(frame_pil)
-                frame_with_text = add_text_to_frame(frame_pil, quote, font, (original_width, original_height), duration, video_length)
+                frame_with_text = add_text_to_frame(
+                    frame_pil,
+                    quote,
+                    font,
+                    (original_width, original_height),
+                    duration,
+                    video_length,
+                )
 
                 out.append_data(np.array(frame_with_text))
                 duration += 1 / fps
