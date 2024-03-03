060afd15921bd4d58d4aade4e82d0f1be0ca809e
aditya-sarawat <adityasarawat@gmail.com>
2024-03-03 16:51:14 +0530

    minor bug fix

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from motivational_content import generate_motivational_content
-import time
 
 if __name__ == "__main__":
-    count = 0
-    while count < 10:
-        generate_motivational_content()
-        time.sleep(20)
-        count += 1
+    generate_motivational_content()
